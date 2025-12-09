00d4c967c94893efd18cd730a6d9480ce7003d71
Tuanhung5211 <phamtuanhung521@gmail.com>
2025-12-09 19:31:46 +0700

    new

--- Ashion/js/custom.js
+++
@@ -1,8 +0,0 @@
-<script>
-$(document).ready(function() {
-            // Sử dụng jQuery để thêm hiệu ứng fade toggle
-            $("#toggleBtn").click(function() {
-                $("#content").fadeToggle(500);
-            })
-        });
-</script>
